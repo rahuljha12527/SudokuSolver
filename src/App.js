8768e7b3d302b5d7d700a43b186d5943939798e4
rahuljha661 <rahuljha12527@gmail.com>
2020-10-04 22:29:11 +0530

    generate modified sudoku

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import generator from "sudoku";
 import "./App.css";
-import SudokuBoard from './componenets/SudokuBoard';
+import produce from "immer";
+import SudokuBoard from "./componenets/SudokuBoard";
 
 window.generator = generator;
 
@@ -10,10 +11,12 @@ function generateSudoku() {
   console.log(raw);
   const result = { rows: [] };
 
+  result.solution = generator.solvepuzzle(raw);
+
   for (let i = 0; i < 9; i++) {
     const row = { cols: [], index: i };
     for (let j = 0; j < 9; j++) {
-      const value = raw[i*9+j];
+      const value = raw[i * 9 + j];
       const col = {
         row: i,
         col: j,
@@ -27,15 +30,49 @@ function generateSudoku() {
   return result;
 }
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       sudoku:generateSudoku()
+function checkSolution(sudoku) {
+  const candidate = sudoku.rows
+    .map((row) => row.cols.map((col) => col.value))
+    .flat();
+  for (let i = 0; i < candidate.length; i++) {
+    if (candidate[i] === null || candidate[i] !== sudoku.solution[i]) {
+      return false;
     }
   }
-  
+  return true;
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = produce({}, () => ({
+      sudoku: generateSudoku(),
+    }));
+  }
+
+  handleChange = (e) => {
+    this.setState(
+      produce((state) => {
+        state.sudoku.rows[e.row].cols[e.col].value = e.value;
+      })
+    );
+  };
+
+  solveSudoku = (e) => {
+    this.setState(
+      produce((state) => {
+        state.sudoku.rows.forEach((row) => {
+          row.cols.forEach((col) => {
+            if (!col.readonly) {
+              col.value = state.sudoku.solution[col.row * 9 + col.col];
+            }
+          });
+        });
+      })
+    );
+  };
+
   render() {
     return (
       <div className="App">
@@ -43,7 +80,9 @@ class App extends Component {
           <h1>Sudoku Solver</h1>
         </header>
 
-        <SudokuBoard sudoku={this.state.sudoku} />
+        <SudokuBoard sudoku={this.state.sudoku} onChange={this.handleChange} />
+
+        <button onClick={this.solveSudoku}>Solve it Magically!</button>
       </div>
     );
   }
--- src/componenets/SudokuBoard.js
+++ src/componenets/SudokuBoard.js
@@ -3,13 +3,13 @@ import SudokuField from "../componenets/SudokuField";
 
 export default class SudokuBoard extends Component {
   render() {
-    const { sudoku } = this.props;
+    const { sudoku,onChange } = this.props;
     return (    
       <div>
         {sudoku.rows.map(row => (
           <div className="row" key={row.index}>     
             {row.cols.map( field => (
-              <SudokuField field={field} key={field.col} />
+              <SudokuField field={field} key={field.col} onChange={onChange}/>
             ))}
           </div>
         ))}
