b0a59fa4b7d3ccbb06a530ff4b29d69f4cda908e
rahuljha661 <rahuljha12527@gmail.com>
2020-10-04 16:38:33 +0530

    add sudoku solver

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import generator from "sudoku";
+import "./App.css";
+import SudokuBoard from './componenets/SudokuBoard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+window.generator = generator;
+
+function generateSudoku() {
+  const raw = generator.makepuzzle();
+  console.log(raw);
+  const result = { row: [] };
+
+  for (let i = 0; i < 9; i++) {
+    const row = { cols: [], index: 1 };
+    for (let j = 0; j < 9; j++) {
+      const value = row[i * 9];
+      const col = {
+        row: i,
+        col: j,
+        value: value,
+        readonly: value !== null,
+      };
+      row.cols.push(col);
+    }
+    result.row.push(row);
+  }
+  return result;
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       sudoku:generateSudoku()
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Sudoku Solver</h1>
+        </header>
+
+        <SudokuBoard sudoku={this.state.sudoku} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/componenets/SudokuBoard.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+
+export default class SudokuBoard extends Component{
+    render(){
+        return <div>
+            {JSON.stringify(this.props.sudoku)}
+        </div>
+    }
+}
